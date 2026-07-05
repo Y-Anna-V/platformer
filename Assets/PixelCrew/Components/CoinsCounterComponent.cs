e4a61e62e87a9751b0ea766d2a792bab8ecad64c
Anna <yarusova.anna@bk.ru>
2026-07-05 07:59:07 +0300

    Первый коммит

---
+++ Assets/PixelCrew/Components/CoinsCounterComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class CoinsCounterComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            coins = 0;
+            Debug.Log(coins);
+        }
+        public static int coins = 0;
+        public void CountCoins()
+        {
+
+            if (tag == "SilverCoin")
+            {
+                coins++;
+            }
+            else if (tag == "GoldenCoin")
+            {
+                coins = coins + 10;
+            }
+            Debug.Log(coins);
+        }
+
+    }
+}
---
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PixelCrew
+{
+    public class HeroInputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _hero.SetDirection(direction);
+        }
+
+        public void OnSaySomething(InputAction.CallbackContext context)
+        {
+            if (context.canceled)
+            {
+                _hero.SaySomething();
+            }
+        }
+    }
+}
